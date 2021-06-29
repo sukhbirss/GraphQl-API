be1c91634c63e284f64dde3cb0d0fae08b38ff23
sukhbirss <s.sukhbirsohal@gmail.com>
2021-06-29 18:07:45 +0530

    create post added

---
+++ backup/graphql/typeDefs.js
@@ -0,0 +1,13 @@
+const { gql } = require('apollo-server');
+
+module.export = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+`;
---
+++ index.js
@@ -0,0 +1,23 @@
+const { ApolloServer } = require('apollo-server');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+mongoose
+  .connect(process.env.DATABASE, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
